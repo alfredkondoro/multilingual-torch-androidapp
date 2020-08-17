67ffedfc2eb2f6793795e35bc167667efb98f59b
Alfred Kondoro <alfredkondoro97@gmail.com>
2020-08-17 17:55:27 +0300

    version 3.0

--- app/src/main/java/com/alfredkondoro/torchapp/MainActivity.java
+++ app/src/main/java/com/alfredkondoro/torchapp/MainActivity.java
@@ -2,14 +2,20 @@ package com.alfredkondoro.torchapp;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.res.Configuration;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
+import android.media.VolumeShaper;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -17,10 +23,13 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton buttontaa;
+    ImageButton flashbutton;
+    Button langbutton;
     private static final int CAMERA_REQUEST = 123;
     boolean CameraFlash = false;
     int FlashStatus = 0;
@@ -28,8 +37,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
+        loadLocale ();
         setContentView (R.layout.activity_main);
 
+        ActionBar actionBar = getSupportActionBar ();
+        actionBar.setTitle (getResources ().getString (R.string.app_name));
 
         ActivityCompat.requestPermissions (MainActivity.this,
                 new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST);
@@ -37,9 +49,10 @@ public class MainActivity extends AppCompatActivity {
         CameraFlash = getPackageManager ().
                 hasSystemFeature (PackageManager.FEATURE_CAMERA_FLASH);
 
-        buttontaa = findViewById (R.id.imageButtonOnOff);
+        flashbutton = findViewById (R.id.imageButtonOnOff);
+        langbutton = findViewById (R.id.langbutton);
 
-        buttontaa.setOnClickListener (new View.OnClickListener () {
+        flashbutton.setOnClickListener (new View.OnClickListener () {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View view) {
@@ -53,13 +66,83 @@ public class MainActivity extends AppCompatActivity {
                         flashLightOn ();
                     }
                 } else {
-                    Toast.makeText (MainActivity.this, "Hakuna flash kwenye chombo chako",
+                    Toast.makeText (MainActivity.this, "@string/app_name_capital",
                             Toast.LENGTH_SHORT).show ();
                 }
             }
         });
+
+        langbutton.setOnClickListener (new View.OnClickListener () {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                showChangeLanguageDialog();
+            }
+        });
+
     }
 
+    private void showChangeLanguageDialog() {
+        final String[] listItems={"English", "French", "German", "Portuguese", "Spanish", "Swahili", "Turkish"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder (MainActivity.this);
+        mBuilder.setTitle ("Choose Language...");
+        mBuilder.setSingleChoiceItems (listItems, -1, new DialogInterface.OnClickListener () {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(i == 0){
+                    setLocale("en");
+                    recreate ();
+                }
+                else if(i == 1){
+                    setLocale("fr");
+                    recreate ();
+                }
+                else if(i == 2){
+                    setLocale("de");
+                    recreate ();
+                }
+                else if(i == 3){
+                    setLocale("pt");
+                    recreate ();
+                }
+                else if(i == 4){
+                    setLocale("es");
+                    recreate ();
+                }
+                else if(i == 5){
+                    setLocale("sw");
+                    recreate ();
+                }
+                else if(i == 6){
+                    setLocale("tr");
+                    recreate ();
+                }
+
+                dialogInterface.dismiss ();
+            }
+        });
+
+        AlertDialog mDialog = mBuilder.create ();
+        mDialog.show ();
+    }
+
+    private void setLocale(String lang) {
+        Locale locale = new Locale (lang);
+        Locale.setDefault (locale);
+        Configuration config = new Configuration ();
+        config.locale = locale;
+        getBaseContext ().getResources ().updateConfiguration (config, getBaseContext ().getResources ().getDisplayMetrics ());
+
+        SharedPreferences.Editor editor = getSharedPreferences ("Settings", MODE_PRIVATE).edit ();
+        editor.putString ("My_Lang", lang);
+        editor.apply ();
+    }
+
+    private void loadLocale(){
+        SharedPreferences prefs = getSharedPreferences ("Settings", Activity.MODE_PRIVATE);
+        String language = prefs.getString ("My_Lang", "");
+        setLocale (language);
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     private void flashLightOn() {
@@ -69,6 +152,7 @@ public class MainActivity extends AppCompatActivity {
             String cameraId = cameraManager.getCameraIdList()[0];
             cameraManager.setTorchMode(cameraId, true);
         } catch (CameraAccessException e) {
+            Toast.makeText(MainActivity.this, "@string/torch_no_on", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -79,22 +163,21 @@ public class MainActivity extends AppCompatActivity {
             String cameraId = cameraManager.getCameraIdList()[0];
             cameraManager.setTorchMode(cameraId, false);
         } catch (CameraAccessException e) {
+            Toast.makeText(MainActivity.this, "@string/torch_no_off", Toast.LENGTH_SHORT).show();
         }
     }
 
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case CAMERA_REQUEST:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    CameraFlash = getPackageManager().
-                            hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-                } else {
-                    buttontaa.setEnabled(false);
-                    Toast.makeText(MainActivity.this, "Ruhusa ya camera imekataliwa", Toast.LENGTH_SHORT).show();
-                }
-                break;
+        if (requestCode == CAMERA_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                CameraFlash = getPackageManager ().
+                        hasSystemFeature (PackageManager.FEATURE_CAMERA_FLASH);
+            } else {
+                flashbutton.setEnabled (false);
+                Toast.makeText (MainActivity.this, "@string/app_permission", Toast.LENGTH_SHORT).show ();
+            }
         }
     }
 }
